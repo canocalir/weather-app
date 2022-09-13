ad760f3fea25ba93ebcb11c3228d5e20a13a6b14
Can Berk Ocalir <can.ocalir@gmail.com>
2022-09-14 01:27:11 +0300

    fixed conditional colors

--- src/components/PollutionBox/PollutionBox.jsx
+++ src/components/PollutionBox/PollutionBox.jsx
@@ -6,15 +6,15 @@ const PollutionBox = () => {
   return (
     <div className='air-box'>
         <div className={
-            airByLocation & searchAir <= 1 
+            airByLocation === 1 || searchAir === 1 
             ? 'air-box__aqi-green'
-            : airByLocation & searchAir <= 2
+            : airByLocation === 2 || searchAir === 2
             ? 'air-box__aqi-yellow'
-            : airByLocation & searchAir <= 3
+            : airByLocation === 3 || searchAir === 3
             ? 'air-box__aqi-orange'
-            : airByLocation & searchAir <= 4
+            : airByLocation === 4 || searchAir === 4
             ? 'air-box__aqi-red'
-            : airByLocation & searchAir <= 5
+            : airByLocation === 5 || searchAir === 5
             ? 'air-box__aqi-purple'
             : 'air-box__aqi-maroon'
         }>
@@ -25,11 +25,11 @@ const PollutionBox = () => {
         }</h2>
         <h3>
             {
-            airByLocation & searchAir <= 1 ? 'Good' :
-            airByLocation & searchAir <= 2 ? 'Moderate' :
-            airByLocation & searchAir <= 3 ? 'Unhealthy-for-sensitive-groups' :
-            airByLocation & searchAir <= 4 ? 'Unhealthy' :
-            airByLocation & searchAir <= 5 ? 'Very-unhealthy' :
+            airByLocation === 1 || searchAir === 1 ? 'Good' :
+            airByLocation === 2 || searchAir === 2 ? 'Moderate' :
+            airByLocation === 3 || searchAir === 3 ? 'Unhealthy-for-sensitive-groups' :
+            airByLocation === 4 || searchAir === 4 ? 'Unhealthy' :
+            airByLocation === 5 || searchAir === 5 ? 'Very-unhealthy' :
             'Hazardous'
         }</h3>
         </div>
